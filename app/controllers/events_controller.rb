0acfcd7976944dff802ae6080db199c8fb04868a
shion1110 <shionn1110@gmail.com>
2022-08-10 17:29:37 +0900

    Revert "first commit"
    This reverts commit 0c006355c8c848da5ae53eb75a5ccccf7bb28071.

--- app/controllers/events_controller.rb
+++
@@ -1,9 +0,0 @@
-class EventsController < ApplicationController
-  def index
-    @events = Event.all
-  end
-
-  def new
-    @event = Event.new
-  end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'events/index' 
-
-  root to: 'events#index'
-  resources :events
-end
